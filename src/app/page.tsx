31032c988e99afed42c2890d5b1d47d608ff703c
bender <kudin.dima@gmail.com>
2026-05-28 12:26:54 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -187,7 +187,7 @@ export default function LandingPage() {
 
   <div id="footer" data-section="footer">
       <FooterMedia
-      imageSrc="http://img.b2bpic.net/free-photo/representation-collective-mind-process-concept-digital-art-style_23-2151657842.jpg"
+      imageSrc="http://img.b2bpic.net/free-photo/representation-collective-mind-process-concept-digital-art-style_23-2151657842.jpg?_wi=1"
       imageAlt="Abstract blue and purple gradient background"
       logoText="FocusFlow"
       columns={[
